f051087d0752bd82c2e4580eec78390e5f86a447
TheRealKevBot <Kglas001@gmail.com>
2021-07-23 14:53:58 -0600

    First

---
+++ index.js
@@ -0,0 +1,10 @@
+//  TenPin Bowling
+//  link: https://www.codewars.com/kata/5531abe4855bcc8d1f00004c/train/javascript
+
+
+function bowlingScore(frames) {
+    // Figure out the score!
+}
+
+console.log(bowlingScore('11 11 11 11 11 11 11 11 11 11'), 20)
+console.log(bowlingScore('X X X X X X X X X XXX'), 300)
